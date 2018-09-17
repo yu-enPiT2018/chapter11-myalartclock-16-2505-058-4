0e8128d326e341a62fcc647ce66d46bf293cdbac
w058ff <w058ff@yamaguchi-u.ac.jp>
2018-09-17 17:19:14 +0900

    キャプチャー１１

---
+++ app/src/main/java/com/example/enpit_p12/myalarmclock/AlarmBroadcastReceiver.kt
@@ -0,0 +1,21 @@
+package com.example.enpit_p12.myalarmclock
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
+import org.jetbrains.anko.toast
+import kotlin.coroutines.experimental.coroutineContext
+
+class AlarmBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+       context?.run {
+           startActivity(
+                   intentFor<MainActivity>("onReceive" to true).newTask())
+           )
+       }
+    }
+
+
+}
